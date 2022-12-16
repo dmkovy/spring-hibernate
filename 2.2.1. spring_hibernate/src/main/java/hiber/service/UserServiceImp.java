d324e508bb073f2ee8f0f86ed8f49703576896a5
Dmitry Kovalchuk <andycoutur@gmail.com>
2022-12-16 16:28:49 +0200

    First commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -26,4 +26,9 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
+   @Transactional(readOnly = true)
+   @Override
+   public User getUserByCarModel(String model, int series) {
+      return userDao.getUserByCarModel(model, series);
+   }
 }
